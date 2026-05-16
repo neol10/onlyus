7c65699ff0858737a042db7183d9d5c0e885046b
neol10 <neol10@github.com>
2026-05-16 12:27:45 -0300

    fix: add missing PWA meta tags for installation support

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#6366f1" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="OnlyUs" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
