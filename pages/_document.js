1df13c18482b075803649a3c9e904ebbd7ce4901
neol10 <neol10@github.com>
2026-05-16 12:45:09 -0300

    fix: modernize pwa manifest for android 14 and play protect

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,17 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#6366f1" />
+        <meta name="theme-color" content="#0f172a" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="OnlyUs" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="application-name" content="OnlyUs" />
+        <meta name="msapplication-TileColor" content="#0f172a" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        
         <script
           dangerouslySetInnerHTML={{
             __html: `
